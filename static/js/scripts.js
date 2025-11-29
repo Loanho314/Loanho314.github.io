d8ac2fdca9dc246c2850736824e2a8953b53309c
LoanHo <96250569+Loanho314@users.noreply.github.com>
2025-11-29 16:30:16 +0100

    Add 'CV' to section names array

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -2,7 +2,7 @@
 
 const content_dir = 'contents/'
 const config_file = 'config.yml'
-const section_names = ['home', 'publications', 'projects']
+const section_names = ['home', 'publications', 'projects', 'CV']
 
 
 window.addEventListener('DOMContentLoaded', event => {
